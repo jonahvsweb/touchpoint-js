a297f1578453e73c6adb46db7a9ed9a68177f167
Jonah Bitautas <jonahvsweb@gmail.com>
2017-09-29 14:47:44 -0500

    Grunt babel build success.

---
+++ dist/touchpoint-es5.js
@@ -0,0 +1,13 @@
+/* 
+ * TouchPoint.js (ES6 compliant) v1.0.1 - 2017-09-29 
+ * A JavaScript library that visually shows taps/cicks on HTML prototypes 
+ * https://github.com/jonahvsweb/touchpoint-js 
+ * 
+ * Copyright (c) 2017 Jonah Bitautas <jonahvsweb@gmail.com> 
+ * 
+ * Released under the MIT license 
+*/
+'use strict';
+
+(function () {})();
+//# sourceMappingURL=touchpoint-es5.js.map
